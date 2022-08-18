497eccfbc73edf41ba4851f1eae363a0fccd8d8e
thiagosantos346 <thiagosantos346@gmail.com>
2022-08-18 14:19:40 -0300

    configurações iniciais do app

---
+++ lib/modules/home/price_controller_home_page.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/widgets.dart';
+import './price_controller_home_state.dart';
+
+class PriceControllerHomePage extends StatefulWidget {
+  const PriceControllerHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<PriceControllerHomePage> createState() => PriceControllerHomeState();
+}
