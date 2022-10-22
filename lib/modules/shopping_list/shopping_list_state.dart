b12912dbec86b5a9e7e7080a1ca7a0b9b39f8d69
thiagosantos346 <thiagosantos346@gmail.com>
2022-10-22 15:18:17 -0300

    ajustado para funcionar também quando deletar todos os itens da lista

--- lib/modules/shopping_list/shopping_list_state.dart
+++ lib/modules/shopping_list/shopping_list_state.dart
@@ -47,37 +47,49 @@ class ShoppingListState extends State<ShoppingListPage> {
           ],
         ),
       ),
-      body: ListView.builder(
-        itemCount: repository.length,
-        padding: _listEdges,
-        itemBuilder: (BuildContext context, int index) {
-          return CardCustom(
-            textValueListName:
-                repository.getAllShoppingListItems().elementAt(index).listName,
-            textValueCreatedAt: DateFormat.yMd().format(repository
-                .getAllShoppingListItems()
-                .elementAt(index)
-                .createdAt),
-            textValueStatus: repository
-                .getAllShoppingListItems()
-                .elementAt(index)
-                .statusToString(),
-            textValueTotalList: repository
-                .getAllShoppingListItems()
-                .elementAt(index)
-                .totalValue
-                .toString(),
-            onPressedDeleteButton: () => {
-              setState(() {
-                repository.delete(index);
-              })
-            },
-          );
-        },
-      ),
+      body: (repository.length > 0)
+          ? ListView.builder(
+              itemCount: repository.length,
+              padding: _listEdges,
+              itemBuilder: (BuildContext context, int index) {
+                return CardCustom(
+                  textValueListName: repository
+                      .getAllShoppingListItems()
+                      .elementAt(index)
+                      .listName,
+                  textValueCreatedAt: DateFormat.yMd().format(repository
+                      .getAllShoppingListItems()
+                      .elementAt(index)
+                      .createdAt),
+                  textValueStatus: repository
+                      .getAllShoppingListItems()
+                      .elementAt(index)
+                      .statusToString(),
+                  textValueTotalList: repository
+                      .getAllShoppingListItems()
+                      .elementAt(index)
+                      .totalValue
+                      .toString(),
+                  onPressedDeleteButton: () => {
+                    setState(() {
+                      repository.delete(index);
+                    })
+                  },
+                );
+              },
+            )
+          : Center(
+              child: BottomAppBarAddButton(
+                repository: repository,
+                onBody: true,
+              ),
+            ),
       bottomNavigationBar: BottomAppBar(
         color: PersonalColors.colorPrimaryVariant,
-        child: BottomAppBarAddButton(repository: repository),
+        child: BottomAppBarAddButton(
+          repository: repository,
+          onBody: false,
+        ),
       ),
     );
   }
@@ -87,9 +99,11 @@ class BottomAppBarAddButton extends StatefulWidget {
   const BottomAppBarAddButton({
     Key? key,
     required this.repository,
+    required this.onBody,
   }) : super(key: key);
 
   final ShoppingListRepository repository;
+  final bool onBody;
 
   @override
   State<BottomAppBarAddButton> createState() => _BottomAppBarAddButtonState();
@@ -107,13 +121,15 @@ class _BottomAppBarAddButtonState extends State<BottomAppBarAddButton> {
   Widget build(BuildContext context) {
     return Container(
       height: 45,
+      alignment: Alignment.center,
       decoration: BoxDecoration(
         boxShadow: PersonalDecorations.defaultShadows(),
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          (widget.repository.length == 0)
+          (widget.repository.length == 0 && !widget.onBody)
               ? Text(
                   'Não existem Listas de compra',
                   style: FontStyle.fontTextCleanBoldBig,
@@ -127,168 +143,158 @@ class _BottomAppBarAddButtonState extends State<BottomAppBarAddButton> {
                     "Criar nova lista",
                     style: FontStyle.fontTextCleanBoldBig,
                   ),
-                  onPressed: () => {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return Dialog(
-                          backgroundColor: PersonalColors.colorPrimary,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          elevation: 16,
-                          child: Form(
-                            key: _formKey,
-                            child: SizedBox(
-                              height: 255,
-                              width: 500,
-                              child: Padding(
-                                padding: const EdgeInsets.all(10),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text(
-                                      "Nome da lista",
-                                      style: FontStyle.fontTextCleanBold,
-                                    ),
-                                    SizedBox(
-                                      height: 40,
-                                      width: 350,
-                                      child: TextFieldCustom(
-                                        '',
-                                        controller: _listNameController,
-                                        withLabel: false,
-                                        inputType: TextInputType.emailAddress,
-                                        validation: (value) {
-                                          return (value!.trim().isEmpty)
-                                              ? "Qual é o nome da lista?"
-                                              : null;
-                                        },
-                                      ),
-                                    ),
-                                    Row(
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              0, 10, 0, 10),
-                                          child: SizedBox(
-                                            height: 70,
-                                            width: 70,
-                                            child: PersonalIcons.assaiIco(),
-                                          ),
-                                        ),
-                                        Padding(
-                                            padding: const EdgeInsets.fromLTRB(
-                                                10, 0, 0, 0),
-                                            child: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.start,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  "Local",
-                                                  style: FontStyle
-                                                      .fontTextCleanBold,
-                                                ),
-                                                SizedBox(
-                                                  width: 212,
-                                                  height: 40,
-                                                  child: TextFieldCustom(
-                                                    '',
-                                                    controller:
-                                                        _listLocalController,
-                                                    withLabel: false,
-                                                    inputType:
-                                                        TextInputType.text,
-                                                    validation: (value) {
-                                                      return (value!
-                                                              .trim()
-                                                              .isEmpty)
-                                                          ? "Onde você vai comprar?"
-                                                          : null;
-                                                    },
-                                                  ),
-                                                ),
-                                              ],
-                                            ))
-                                      ],
-                                    ),
-                                    const Divider(
-                                      height: 5,
-                                      color: PersonalColors.colorOnPrimary,
-                                      thickness: 1,
-                                    ),
-                                    ElevatedButtonCustom(
-                                      edges: _itemListEdges,
-                                      heigth: 35,
-                                      width: 300,
-                                      color: PersonalColors.colorPrimaryVariant,
-                                      alignment: Alignment.center,
-                                      onPressed: () {
-                                        if (_formKey.currentState!.validate()) {
-                                          widget.repository.addShoppingList(
-                                            shoppingList: ShoppingListModel(
-                                              listName:
-                                                  _listNameController.text,
-                                              marketName:
-                                                  _listLocalController.text,
-                                              createdAt: DateTime.now(),
-                                              status:
-                                                  ShoppingListStatus.unarchived,
-                                              totalValue: 0,
-                                            ),
-                                          );
-                                          _listNameController.clear();
-                                          _listLocalController.clear();
-                                          Navigator.pop(context, false);
-                                        }
-                                      },
-                                      label: Text(
-                                        'Confirmar',
-                                        style: FontStyle.fontTextClean,
-                                      ),
-                                      icon: const Icon(
-                                        Icons.check,
-                                        size: 20,
-                                      ),
-                                    ),
-                                    ElevatedButtonCustom(
-                                      edges: _itemListEdges,
-                                      heigth: 35,
-                                      width: 300,
-                                      alignment: Alignment.center,
-                                      color: PersonalColors.colorPrimaryVariant,
-                                      onPressed: () {
-                                        setState(() {
-                                          _listNameController.clear();
-                                          _listLocalController.clear();
-                                          Navigator.pop(context, false);
-                                        });
-                                      },
-                                      label: Text(
-                                        'Cancelar',
-                                        style: FontStyle.fontTextClean,
-                                      ),
-                                      icon: const Icon(
-                                        Icons.close,
-                                        size: 20,
-                                      ),
-                                      //icon: const Icon(Icons.close),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    )
-                  },
+                  onPressed: () => {buildShowDialogCreateShoppingList(context)},
                 ),
         ],
       ),
+    );
+  }
+
+  Future<dynamic> buildShowDialogCreateShoppingList(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          backgroundColor: PersonalColors.colorPrimary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          elevation: 16,
+          child: Form(
+            key: _formKey,
+            child: SizedBox(
+              height: 255,
+              width: 500,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Nome da lista",
+                      style: FontStyle.fontTextCleanBold,
+                    ),
+                    SizedBox(
+                      height: 40,
+                      width: 350,
+                      child: TextFieldCustom(
+                        '',
+                        controller: _listNameController,
+                        withLabel: false,
+                        inputType: TextInputType.emailAddress,
+                        validation: (value) {
+                          return (value!.trim().isEmpty)
+                              ? "Qual é o nome da lista?"
+                              : null;
+                        },
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: SizedBox(
+                            height: 70,
+                            width: 70,
+                            child: PersonalIcons.assaiIco(),
+                          ),
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Local",
+                                  style: FontStyle.fontTextCleanBold,
+                                ),
+                                SizedBox(
+                                  width: 212,
+                                  height: 40,
+                                  child: TextFieldCustom(
+                                    '',
+                                    controller: _listLocalController,
+                                    withLabel: false,
+                                    inputType: TextInputType.text,
+                                    validation: (value) {
+                                      return (value!.trim().isEmpty)
+                                          ? "Onde você vai comprar?"
+                                          : null;
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ))
+                      ],
+                    ),
+                    const Divider(
+                      height: 5,
+                      color: PersonalColors.colorOnPrimary,
+                      thickness: 1,
+                    ),
+                    ElevatedButtonCustom(
+                      edges: _itemListEdges,
+                      heigth: 35,
+                      width: 300,
+                      color: PersonalColors.colorPrimaryVariant,
+                      alignment: Alignment.center,
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          widget.repository.addShoppingList(
+                            shoppingList: ShoppingListModel(
+                              listName: _listNameController.text,
+                              marketName: _listLocalController.text,
+                              createdAt: DateTime.now(),
+                              status: ShoppingListStatus.unarchived,
+                              totalValue: 0,
+                            ),
+                          );
+                          _listNameController.clear();
+                          _listLocalController.clear();
+                          Navigator.pop(context, false);
+                        }
+                      },
+                      label: Text(
+                        'Confirmar',
+                        style: FontStyle.fontTextClean,
+                      ),
+                      icon: const Icon(
+                        Icons.check,
+                        size: 20,
+                      ),
+                    ),
+                    ElevatedButtonCustom(
+                      edges: _itemListEdges,
+                      heigth: 35,
+                      width: 300,
+                      alignment: Alignment.center,
+                      color: PersonalColors.colorPrimaryVariant,
+                      onPressed: () {
+                        setState(() {
+                          _listNameController.clear();
+                          _listLocalController.clear();
+                          Navigator.pop(context, false);
+                        });
+                      },
+                      label: Text(
+                        'Cancelar',
+                        style: FontStyle.fontTextClean,
+                      ),
+                      icon: const Icon(
+                        Icons.close,
+                        size: 20,
+                      ),
+                      //icon: const Icon(Icons.close),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
